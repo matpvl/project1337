45ff16104bc62cf138b337accc58ead85e0d4da7
Matija Pavlovic <matija.pavlovic@wagalabs.com>
2024-10-28 19:28:31 +0100

    added invoke + tasks.py which we will run for the local build in the future, updated poetry with several packages, mostly linters and such

---
+++ tasks.py
@@ -0,0 +1,40 @@
+from invoke import task
+
+@task
+def ruff(c):
+    """Run ruff linter."""
+    c.run("ruff check", pty=True)
+
+@task
+def ruff_format(c):
+    """Format using ruff linter."""
+    c.run("ruff format", pty=True)
+
+@task
+def mypy(c):
+    """Run mypy type checks."""
+    c.run("mypy .", pty=True)
+
+
+@task
+def black(c):
+    """Run black to format code."""
+    c.run("black .", pty=True)
+
+
+@task
+def radon_cc(c):
+    """Run Radon to compute cyclomatic complexity."""
+    c.run("radon cc . -a", pty=True)
+
+
+@task
+def radon_mi(c):
+    """Run Radon to compute maintainability index."""
+    c.run("radon mi .", pty=True)
+
+
+@task(pre=[mypy, black, radon_cc, radon_mi])
+def lint(c):
+    """Run all tasks: mypy, black, and radon."""
+    print("All checks completed.")
