8cad268a2fc79afa66590441f9a876d4e26dc7c5
Matija Pavlovic <matija.pavlovic@wagalabs.com>
2024-10-29 12:52:47 +0100

    changed name of the file containing celery tasks to tasks.py for convention, optimized imports, added broker to app instance

--- project1337/__init__.py
+++ project1337/__init__.py
@@ -1,5 +1,5 @@
 """Project 1337."""
 
-from celery import app as celery_app
+from .celery import app as celery_app
 
 __all__ = ("celery_app",)
--- project1337/celery.py
+++ project1337/celery.py
@@ -2,11 +2,12 @@
 
 import os
 
+from django.conf import settings
 from celery.app import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project1337.settings")
 
-app = Celery("project1337")
+app = Celery("project1337", broker=settings.CELERY_BROKER_URL)
 
 # Read config from Django settings with the 'CELERY' prefix.
 app.config_from_object("django.conf:settings", namespace="CELERY")
