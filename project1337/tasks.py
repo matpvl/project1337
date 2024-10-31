ed61ede1645a2eddee86349fcd164039834e3ebc
Matija Pavlovic <matija.pavlovic@wagalabs.com>
2024-10-31 16:22:11 +0100

    updated the celery task to timestamp the beginnign and end of phone call + updated the django model with these 2 fields

--- project1337/tasks.py
+++ project1337/tasks.py
@@ -2,6 +2,7 @@
 
 import time
 from celery import shared_task
+from django.utils import timezone
 
 from telemarketing.models import TargetedContact
 from telemarketing.quote_generation import miss_the_old_kanye
@@ -16,8 +17,14 @@ def add(x, y):
 @shared_task
 def contact_person_via_phone(contact_id):
     """Simulate a phone call that takes 120 seconds and update DB status."""
-    time.sleep(120)  # Simulate long-running task
     contact = TargetedContact.objects.get(id=contact_id)
+    contact.call_started_at = timezone.now()
+    contact.save(update_fields=["call_started_at"])
+
+    # Simulate a 2 minute phone call
+    time.sleep(120)
+
     contact.reply = miss_the_old_kanye()
     contact.status = "DONE"
-    contact.save()
+    contact.call_ended_at = timezone.now()
+    contact.save(update_fields=["reply", "status", "call_ended_at"])
--- telemarketing/models.py
+++ telemarketing/models.py
@@ -9,6 +9,8 @@ class TargetedContact(models.Model):
     phone_number = models.CharField(max_length=15, unique=True)
     reply = models.CharField(max_length=255, blank=True, default="")
     status = models.CharField(max_length=20, default="PENDING")
+    call_started_at = models.DateTimeField(null=True, blank=True)
+    call_ended_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         """Return string representation of Targeted Contact."""
